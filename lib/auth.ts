c20e7e0f10865f4556f94f55ad2bec5d6f6c73a5
Ghoui <flareonverm@outlook.com>
2025-12-01 14:59:40 +0000

    sqlitedb, better auth api

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,5 +2,8 @@ import { betterAuth } from "better-auth";
 import Database from "better-sqlite3";
 
 export const auth = betterAuth({
-  database: new Database(""),
+  database: new Database("./sqlite.db"),
+  emailAndPassword: {
+    enabled: true,
+  },
 });
